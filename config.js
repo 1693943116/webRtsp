be00d9eea9df93025ea4debf17b6ee9185abc614
jiangjlf <18755723190@163.com>
2022-01-12 10:19:02 +0800

    添加注释

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-var webrtcServerAddress = "http://localhost:8000"; // 上线要换成公司rtsp服务地址
+var webrtcServerAddress = "http://localhost:8000"; // 上线要换成公司rtsp服务地址  本地双击webrtc-streamer.exe默认开启8000端口
 var preloadTime = 2000; // 历史视频的预加载时间
